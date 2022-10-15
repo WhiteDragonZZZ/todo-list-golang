e89d9e16df09ae6f6d019f9207f771a645d17f28
Zhass <zhass.akimbayev@gmail.com>
2022-10-15 22:26:40 +0600

    Complete Auth and Middleware

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -7,6 +7,7 @@ import (
 
 type Authorization interface {
 	CreateUser(user todo_list_golang.User) (int, error)
+	GetUser(username, password string) (todo_list_golang.User, error)
 }
 
 type TodoList interface {
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -2,12 +2,24 @@ package service
 
 import (
 	"crypto/sha1"
+	"errors"
 	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"time"
 	todo_list_golang "todo-list-golang"
 	"todo-list-golang/pkg/repository"
 )
 
-const salt = "jhbfajhfdshfhladk"
+const (
+	salt       = "jhbfajhfdshfhladk"
+	signingKey = "jsgjgfdjfsjglgsf"
+	tokenTTL   = 12 * time.Hour
+)
+
+type TokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -22,9 +34,42 @@ func (s *AuthService) CreateUser(user todo_list_golang.User) (int, error) {
 	return s.repo.CreateUser(user)
 }
 
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, s.generatedPasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &TokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+	return token.SignedString([]byte(signingKey))
+}
+
 func (s *AuthService) generatedPasswordHash(password string) string {
 	hash := sha1.New()
 	hash.Write([]byte(password))
 
 	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
+
+func (s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &TokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	claims, ok := token.Claims.(*TokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not type *tokenClaims*")
+	}
+	return claims.UserId, nil
 }
